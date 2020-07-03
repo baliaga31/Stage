4847ec86555c63b6c217768f441acfa966915da9
wanaga3166 <aliaga.benoit@gmail.com>
2020-07-03 16:52:35 +0200

    fin de stage

--- cinema.sql
+++ cinema.sql
@@ -26,6 +26,98 @@ SELECT titre, length(titre) as long_titre from film;
 
 -- calcul numerique :
 Select titre, duree, duree/60, duree%60 from film where duree is not NULL;
-SELECT titre, duree/60 as dureeH, duree%60 as dureeM from film where duree is not NULL;
 
--- 
+--- Temporal data
+SELECT * from individu WHERE date_naissance is not NULL;
+set LANG = fr_FR
+set DATESTYLE = SQL, DMY;
+set LC_TIME = fr_FR
+SELECT * from individu WHERE date_naissance is not NULL;
+SELECT prenom, nom, to_char(date_naissance, 'day Mon DD"th" YYYY') from individu WHERE individu is not NULL;
+SELECT prenom, nom, to_char(date_naissance, 'TMday DD TMmon YYYY') from individu WHERE individu is not NULL;
+
+SELECT prenom, nom, extract(year from date_naissance) as annee_naissance 
+from individu 
+WHERE date_naissance is not NULL;
+
+SELECT CURRENT_DATE;
+
+SELECT 
+	*, 
+	extract(year from current_date) - extract(year from date_naissance) as annee_naissance 
+from individu 
+WHERE date_naissance is not NULL;
+
+SELECT 
+	count (*) as nb_film, 
+	count(duree) as nb_duree, 
+	round(avg(duree), 2) as avg_duree 
+from film 
+where annee between 1990 and 2000;
+
+SELECT 
+	count (*) as nb_film, 
+	count(duree) as nb_duree, 
+	round(avg(duree), 2) as avg_duree,
+	sum(duree) as tot_duree
+from film 
+where annee between 2020 and 2029;
+
+SELECT 
+	count (*) as nb_film, 
+	count(duree) as nb_duree, 
+	round(avg(duree), 2) as avg_duree,
+	coalesce(sum(duree),0) as tot_duree
+from film 
+where annee between 2020 and 2029;
+
+SELECT
+	count(*) as nb_film,
+	count(annee) as nb_annee
+	count(distinct annee) as nb_annee_uniq
+from film;
+
+-- Titre et nombre d'annee des films
+SELECT annee, count(*) as nb_film, round(avg(duree),2) as moyenne_duree_film from film
+	Group By annee
+	Order by annee DESC;
+	
+SELECT *
+from film
+Order by annee DESC, titre;
+
+-- regrouper les films par realisateurs
+
+SELECT  
+	annee,
+	num_real,
+	sum(duree) as duree_totale
+from film 
+group by num_real, annee
+order by annee DESC;
+
+SELECT
+	annee,
+	genre,
+	count(num_film) as nombre_film,
+	coalesce(round(avg(duree),2),0) as duree_moyenne_film
+from film
+group by genre, annee
+order by annee;
+
+SELECT max(count(num_film))
+from film
+group by num_real;
+
+
+SELECT max(nb_film) as max_nb_real
+from (SELECT count(num_film) as nb_film
+from film
+group by num_real) nbs;
+
+SELECT num_real, count(num_film) as nb_film
+from film
+group by num_real
+having count(num_film) = (
+	select max(nb_film as max_nb_real
+	from (select count9)))
